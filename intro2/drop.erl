5a4527c65d1e4874343d09d515f38bd64ff1ea54
pahihu <pahihu@gmail.com>
2022-03-15 14:09:45 +0100

    IntroErlang ch9

--- intro2/drop.erl
+++ intro2/drop.erl
@@ -1,5 +1,5 @@
 -module(drop).
--export([drop/0]).
+-export([drop/0, fall_velocity/2]).
 
 drop() ->
    receive
@@ -8,11 +8,20 @@ drop() ->
       drop()
    end.
 
-fall_velocity(Planemo, Distance) when Distance >= 0 ->
-   Gravity = case Planemo of
-      earth -> 9.8;
-      moon  -> 1.6;
-      mars  -> 3.71;
-      X     -> 9.8
-   end,
-   math:sqrt(2 * Gravity * Distance).
+fall_velocity(Planemo, Distance) ->
+   try
+      Gravity = case Planemo of
+         earth -> 9.8;
+         moon  -> 1.6;
+         mars  -> 3.71;
+            X  -> throw({unknown, X})
+      end,
+      math:sqrt(2 * Gravity * Distance)
+   of
+      Result -> Result
+   catch
+      error:Error     -> { found, Error};
+      throw:Exception -> {caught, Exception}
+   after
+      error_logger:info_msg("fall_velocity done~n")
+   end.

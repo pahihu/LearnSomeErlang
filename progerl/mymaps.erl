5ce2fb6d170e7f8fc2977418590933d1995b8530
Andras Pahi <pahia@t-online.hu>
2022-04-08 11:56:15 +0200

    ProgErl mymaps:to_json,from_json

--- progerl/mymaps.erl
+++ progerl/mymaps.erl
@@ -1,6 +1,6 @@
 -module(mymaps).
 -export([count_characters/1, test/0]).
--export([to_json/1]).
+-export([to_json/1, from_json/1]).
 
 count_characters(Str) ->
    count_characters(Str, #{}).
@@ -37,10 +37,10 @@ any_to_json(X) ->
          atom_to_list(X);
       is_integer(X) ->
          integer_to_list(X);
-      is_binary(X) ->
-         "\"" ++ binary_to_list(X) ++ "\"";
       is_float(X) ->
          float_to_list(X, [compact]);
+      is_binary(X) ->
+         "\"" ++ binary_to_list(X) ++ "\"";
       is_list(X) ->
          L = lists:foldr(fun(E,A) ->
                            [any_to_json(E) | comma(A)]
@@ -54,6 +54,108 @@ any_to_json(X) ->
                        [], X),
          [ "{", M, "}" ]
    end.
+
+skip(X) -> string:trim(X, leading).
+
+from_json([], A) ->
+   lists:reverse(A);
+from_json([$ | T], A) ->
+   from_json(skip(T), A);
+from_json(X, A) ->
+   {V, R} = json_to_any(X),
+   from_json(R, [V | A]).
+
+from_json(X) ->
+   from_json(string:trim(X), []).
+
+char_letter(X) -> ($a =< X) andalso (X =< $z).
+char_number(X) ->
+   (($0 =< X) andalso (X =< $9))
+   orelse (X =:= $+)
+   orelse (X =:= $-)
+   orelse (X =:= $E)
+   orelse (X =:= $e)
+   orelse (X =:= $.).
+char_atom(X)  -> char_letter(X) orelse char_number(X).
+char_notquote(X) -> $" /= X.
+
+
+xtakewhile(P, L) ->
+   xtakewhile(P, skip(L), []).
+
+xtakewhile(_P, [], A) ->
+   {lists:reverse(A), []};
+xtakewhile(P, L = [H|T], A) ->
+   case P(H) of
+      true  -> xtakewhile(P, T, [H|A]);
+      false -> {lists:reverse(A), L}
+   end.
+   
+to_number(X) ->
+   case string:to_float(X) of
+      {error, _Reason} ->
+         {I, []} = string:to_integer(X),
+         I;
+      {F, []} ->
+         F
+   end.
+
+parse_atom(L) ->
+   {S, R} = xtakewhile(fun char_atom/1, L),
+   {list_to_existing_atom(S), R}.
+
+parse_number(L) ->
+   {S, R} = xtakewhile(fun char_number/1, L),
+   {to_number(S), R}.
+
+parse_binary(L) ->
+   {S, [$" | R]} = xtakewhile(fun char_notquote/1, L),
+   {list_to_binary(S), R}.
+
+parse_key(L) ->
+   {S, [$" | R]} = xtakewhile(fun char_notquote/1, L),
+   {list_to_existing_atom(S), R}.
+
+
+parse_list([$] | T], A) ->
+   {lists:reverse(A), T};
+parse_list([$ | T], A) ->
+   parse_list(skip(T), A);
+parse_list([$, | T], A) ->
+   parse_list(T, A);
+parse_list(L, A) ->
+   {V, R} = json_to_any(L),
+   parse_list(R, [V | A]).
+
+
+parse_map([$} | T], M) ->
+   {M, T};
+parse_map([$ | T], M) ->
+   parse_map(skip(T), M);
+parse_map([$, | T], M) ->
+   parse_map(T, M);
+parse_map([$" | T], M) ->
+   {K, R1} = parse_key(T),
+   [$: | R2] = skip(R1),
+   {V, R3} = json_to_any(skip(R2)),
+   parse_map(R3, M#{ K => V}).
+   
+
+json_to_any(L = [H|T]) ->
+   if
+      ($a =< H) andalso (H =< $z) ->
+         parse_atom(L);
+      ($0 =< H andalso H =< $9) orelse (H =:= $-) ->
+         parse_number(L);
+      $" =:= H ->
+         parse_binary(T);
+      $[ =:= H ->
+         parse_list(T, []);
+      ${ =:= H ->
+         parse_map(T, #{})
+   end;
+json_to_any([]) ->
+   ok.
 
 
 test() ->
